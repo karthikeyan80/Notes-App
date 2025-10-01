80e7beab8704f9f5bc10ee6cf1ad6fa08db424d0
karthikeyan80 <karthikeyan40581@gmail.com>
2025-10-01 12:05:22 +0530

    completed the backend

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,16 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 
 const app = express();
-
+const PORT = process.env.PORT || 5000 ;
 dotenv.config();
-connectDB();
+
 app.use(express.json());
 app.use("/api/notes", novelRoutes);
 app.use("/", (req, res) => {
   res.status(200).send("The Server Is On");
 });
 
-app.listen(5000, () => {
+connectDB().then(()=>{
+  app.listen(PORT, () => {
   console.log("The Server is running on PORT 5000");
-});
+})})
